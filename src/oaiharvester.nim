4b20296b495a6def88606cd93e3d94d493773af8
Mark Baggett <mbagget1@utk.edu>
2020-10-21 13:08:17 -0400

    Add option to harvest just one set.

--- src/oaiharvester.nim
+++ src/oaiharvester.nim
@@ -13,12 +13,19 @@ proc process_sets(oai_connection: OaiRequest, a_date, metadata_prefix: string): 
   bar.finish()
   len(sets)
 
+proc process_set(oai_connection: OaiRequest, a_date, metadata_prefix: string, oaiset: string): string =
+  echo fmt"{'\n'}Harvesting all records since {a_date} as {metadata_prefix} from set {oaiset}.{'\n'}"
+  oai_connection.oai_set = oaiset
+  discard oai_connection.harvest_metadata_records(metadata_format=metadata_prefix, output_directory=fmt"{getCurrentDir()}/{oaiset}", identifier=true, from_date=a_date, replace_filename="oai:trace.tennessee.edu:")
+  "Done."
+
 when isMainModule:
   var
     p = newParser(fmt"Harvest  OAI Records"):
       option("-d", "--date", help="Download records since this date as yyyy-MM-dd.  Defaults to yesterday.", default="yesterday")
       option("-m", "--metadata_format", help="Specifiy metadata format.", default="qdc")
       option("-e", "--endpoint", help="Specify OAI Endpoint")
+      option("-s", "--oaiset", help="Specify OAI Set")
     argv = commandLineParams()
     opts = p.parse(argv)
     datestring: string
@@ -30,6 +37,9 @@ when isMainModule:
       datestring = $(now() - 1.days)
     else:
       datestring = opts.date
-    discard process_sets(harvester, datestring, opts.metadata_format)
+    if opts.oaiset == "":
+      discard process_sets(harvester, datestring, opts.metadata_format)
+    else:
+      discard process_set(harvester, datestring, opts.metadata_format, opts.oaiset)
   else:
     echo "You must specify an OAI endpoint!"
